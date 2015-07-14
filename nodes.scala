4617e9d8bb0699afd24e29a48d36811a7a7b4478
Alex Jeffery <apjeffery136@googlemail.com>
2015-07-14 10:24:52 +0100

    Made Context a class

--- nodes.scala
+++ nodes.scala
@@ -1,31 +1,79 @@
 type Name = String
 type Prog = Map[Name, List[Statement]]
+
 abstract class Statement
 case class Skip extends Statement
 case class Input(name: Name, node: Name) extends Statement
 case class Output(exp: Expression, node: Name) extends Statement
 case class While(exp: Expression, stmts: List[Statement]) extends Statement
 case class Assign(name: Name, exp: Expression) extends Statement
+
 abstract class Expression
 case class Variable(name: Name) extends Expression
 case class EmptyList extends Expression
 case class Cons(hd: Expression, tl: Expression) extends Expression
 case class Head(exp: Expression) extends Expression
 case class Tail(exp: Expression) extends Expression
+
 type Store = Map[Name, Expression]
-type MessageQueue = Map[Name, Expression]
+type MessageQueue = Map[Name, List[Expression]]
+
 class Context(map: Map[Name, (List[Statement], MessageQueue, Store)]) {
-  def statementGet(nodeName: Name): (Context, Statement) =
+  def statementGet(nodeName: Name): Option[(Context, Statement)] = 
+    this.map.get(nodeName) match {
+      case None => throw new RuntimeException("Node name not in context")
+      case Some((Nil, _, _)) => None
+      case Some((stmt :: stmts, msgQ, store)) =>
+        Some((Context(this.map + (nodeName, (stmts, msgQ, store))), stmt))
+    }
   def statementPut(nodeName: Name, stmt: Statement): Context =
-  def messageGet(rcvrName: Name, sndrName: Name): (Context, Expression) =
+    this.map.get(nodeName) match {
+      case None => throw new RuntimeException("Node name not in context")
+      case Some((stmts, msgQ, store)) =>
+        Context(this.map + (nodeName, (stmt :: stmts, msgQ, store)))
+    }
+  def messageGet(rcvrName: Name, sndrName: Name): Option[(Context, Expression)] =
+    this.map.get(rcvrName) match {
+      case None => throw new RuntimeException("Node name not in context")
+      case Some((stmts, msgQ, store)) => msgQ.get(sndrName) match {
+        case None => throw new
+          RuntimeException("Node name not in message queue")
+        case Some(Nil) => None
+        case Some(msg :: msgs) => Some((Context(this.map +
+          (rcvrName, (stmts, msgQ + (sndrName, msgs), store))), msg))
+      }
+    }
   def messagePut(rcvrName: Name, sndrName: Name, msg: Expression): Context =
-  def varLookup(nodeName: Name, varName: Name): Expression =
-  def varInsert(nodeName: Name, varName: Name, exp: Expression): Context =
+    this.map.get(rcvrName) match {
+      case None => throw new RuntimeException("Node name not in context")
+      case Some((stmts, msgQ, store)) => msgQ.get(sndrName) match {
+        case None => throw new
+          RuntimeException("Node name not in message queue")
+        case Some(msgs) => Context(this.map +
+          (rcvrName, (stmts, msgQ + (sndrName, msgs ++ List(msg)), store)))
+      }
+    }
+  def varGet(nodeName: Name, varName: Name): Expression =
+    this.map.get(nodeName) match {
+      case None => throw new RuntimeException("Node name not in context")
+      case Some((_, _, store)) => store.get(varName) match {
+        case None => EmptyList
+        case Some(exp) => exp
+      }
+    }
+  def varPut(nodeName: Name, varName: Name, exp: Expression): Context =
+    this.map.get(nodeName) match {
+      case None => throw new RuntimeException("Node name not in context")
+      case Some((stmts, msgQ, store)) =>
+        Context(this.map + (nodeName, (stmts, msgQ, store + (varName, exp))))
+    }
 }
+
 object Nodes {
-  def evalExpression(ctx: Context, exp: Expression): Expression =
+
+  def evalExpression(ctx: Context, curNode: Name, exp: Expression): Expression =
     exp match {
-      case Variable(varName) => store(node)(varName)
+      case Variable(varName) => ctx.varGet(curNode, varName)
       case EmptyList => exp
       case Cons(a, b) => Cons(evalExpression(a), evalExpression(b))
       case Head(EmptyList) => EmptyList
@@ -35,6 +83,7 @@ object Nodes {
       case Tail(Cons(hd, tl)) => evalExpression(tl)
       case Tail(other) => evalExpression(Tail(evalExpression(other)))
   }
+
   def evalStatement(ctx: Context, curNode: Name, stmt: Statement): Context =
     stmt match {
       case Skip => ctx
