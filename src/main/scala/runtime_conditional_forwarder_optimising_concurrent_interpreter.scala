28ee6120ba5d15581b30d71e1b47ace56ba7bad3
Alex Jeffery <apjeffery136@googlemail.com>
2015-11-25 17:09:25 +0000

    Continued runtime stuff

--- src/main/scala/runtime_conditional_forwarder_optimising_concurrent_interpreter.scala
+++ src/main/scala/runtime_conditional_forwarder_optimising_concurrent_interpreter.scala
@@ -19,3 +19,46 @@ class RunTCondFwdOptProcRunner(
 
   override def handleServer(chExp: Exp, bind: Name, p: Proc): Unit = ???
 }
+
+object serverRewrite extends Function1[Proc, Option[Proc]] {
+
+  override def apply(p: Proc): Proc = p match {
+    case Send(chExp, msg, p) => serverRewrite(p) map (p => Send(chExp, msg, p))
+
+    case Receive(repl, chExp, bind, p) =>
+      serverRewrite(p) map (p => Receive(repl, chExp, bind, p))
+
+    case LetIn(bind, exp, p) => serverRewrite(p) map (p => LetIn(bind, exp, p))
+
+    case IfThenElse(exp, p, q) =>
+      serverRewrite(p) map (p =>
+        serverRewrite(q) map (q =>
+          IfThenElse(exp, p, q)))
+
+    case Parallel(p, q) =>
+      serverRewrite(p) map (p =>
+        serverRewrite(q) map (q =>
+          Parallel(exp, p, q)))
+
+    case New(nrch0,
+         Send(chExp0, msg0,
+         Receive(false, Variable(nrch1), reply0,
+         IfThenElse(condExp,
+           Send(orch0, Variable(reply1), End),
+           Send(orch1, Variable(reply2), End)))))
+             if ((msg0 contains nrch0)
+             && (nrch0 == nrch1)
+             && (orch0 == orch1)
+             && (reply0 == reply1)
+             && (reply0 == reply2))
+               => (Some(???): Option[Proc])
+
+    case New(bind, p) => serverRewrite(p) map (p => New(bind, p))
+
+    case End => (None: Option[Proc])
+  }
+}
+
+// Signals to a server caller that a new server can be used, so substitute the
+// channel name of the old server with a new one.
+case class UseAlternateChannel(oldCh: Name, newCh: Name) extends MetaInfo
