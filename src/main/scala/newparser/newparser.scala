1bebaa52e69733d4b7f0b6e566e39be2f4d6cb39
Alex Jeffery <apjeffery136@googlemail.com>
2016-10-06 17:47:27 +0300

    Continued new parser

--- src/main/scala/newparser/newparser.scala
+++ src/main/scala/newparser/newparser.scala
@@ -84,3 +84,108 @@ class Parser extends RegexParsers with PackratParsers {
     "<-" ^^ { _ => PLeft  } |
     "->" ^^ { _ => PRight }
 }
+
+class Lexer extends RegexParsers {
+
+  override def skipWhitespace = true
+  override val whiteSpace = "[ \t\r\f\n]+".r
+
+  def lex : Parser [ List [ Token ] ] = phrase ( rep1 (
+    lexBang   | lexStar    | lexDot     | lexColon   | lexLet     |
+    lexNew    | lexIf      | lexThen    | lexElse    | lexEndif   |
+    lexSend   | lexReceive | lexServer  | lexBar     | lexEnd     |
+    lexLparen | lexRparen  | lexLcurly  | lexRcurly  | lexComma   |
+    lexLarrow | lexRarrow  | lexTrue    | lexFalse   | lexPlus    |
+    lexDash   | lexFslash  | lexPerc    | lexEqual   | lexEqeq    |
+    lexNeq    | lexLess    | lexLesseq  | lexGrtr    | lexGrtreq  |
+    lexAnd    | lexOr      | lexIdent   | lexChan    | lexInt     ) )
+
+  def lexBang    : Parser [ Token ] = "!"                  ^^ { _ => BANG    }
+  def lexStar    : Parser [ Token ] = "*"                  ^^ { _ => STAR    }
+  def lexDot     : Parser [ Token ] = "."                  ^^ { _ => DOT     }
+  def lexColon   : Parser [ Token ] = ":"                  ^^ { _ => COLON   }
+  def lexLet     : Parser [ Token ] = "let"                ^^ { _ => LET     }
+  def lexNew     : Parser [ Token ] = "new"                ^^ { _ => NEW     }
+  def lexIf      : Parser [ Token ] = "if"                 ^^ { _ => IF      }
+  def lexThen    : Parser [ Token ] = "then"               ^^ { _ => THEN    }
+  def lexElse    : Parser [ Token ] = "else"               ^^ { _ => ELSE    }
+  def lexEndif   : Parser [ Token ] = "endif"              ^^ { _ => ENDIF   }
+  def lexSend    : Parser [ Token ] = "send"               ^^ { _ => SEND    }
+  def lexReceive : Parser [ Token ] = "receive"            ^^ { _ => RECEIVE }
+  def lexServer  : Parser [ Token ] = "server"             ^^ { _ => SERVER  }
+  def lexBar     : Parser [ Token ] = "|"                  ^^ { _ => BAR     }
+  def lexEnd     : Parser [ Token ] = "end"                ^^ { _ => END     }
+  def lexLparen  : Parser [ Token ] = "("                  ^^ { _ => LPAREN  }
+  def lexRparen  : Parser [ Token ] = ")"                  ^^ { _ => RPAREN  }
+  def lexLcurly  : Parser [ Token ] = "{"                  ^^ { _ => LCURLY  }
+  def lexRcurly  : Parser [ Token ] = "}"                  ^^ { _ => RCURLY  }
+  def lexComma   : Parser [ Token ] = ","                  ^^ { _ => COMMA   }
+  def lexLarrow  : Parser [ Token ] = "<-"                 ^^ { _ => LARROW  }
+  def lexRarrow  : Parser [ Token ] = "->"                 ^^ { _ => RARROW  }
+  def lexTrue    : Parser [ Token ] = "true"               ^^ { _ => TRUE    }
+  def lexFalse   : Parser [ Token ] = "false"              ^^ { _ => FALSE   }
+  def lexPlus    : Parser [ Token ] = "+"                  ^^ { _ => PLUS    }
+  def lexDash    : Parser [ Token ] = "-"                  ^^ { _ => DASH    }
+  def lexFslash  : Parser [ Token ] = "/"                  ^^ { _ => FSLASH  }
+  def lexPerc    : Parser [ Token ] = "%"                  ^^ { _ => PERC    }
+  def lexEqual   : Parser [ Token ] = "="                  ^^ { _ => EQUAL   }
+  def lexEqeq    : Parser [ Token ] = "=="                 ^^ { _ => EQEQ    }
+  def lexNeq     : Parser [ Token ] = "!="                 ^^ { _ => NEQ     }
+  def lexLess    : Parser [ Token ] = "<"                  ^^ { _ => LESS    }
+  def lexLesseq  : Parser [ Token ] = "<="                 ^^ { _ => LESSEQ  }
+  def lexGrtr    : Parser [ Token ] = ">"                  ^^ { _ => GRTR    }
+  def lexGrtreq  : Parser [ Token ] = ">="                 ^^ { _ => GRTREQ  }
+  def lexAnd     : Parser [ Token ] = "&&"                 ^^ { _ => AND     }
+  def lexOr      : Parser [ Token ] = "||"                 ^^ { _ => OR      }
+  def lexIdent   : Parser [ Token ] = """[a-z]+""".r       ^^ { IDENT ( _ )  }
+  def lexChan    : Parser [ Token ] = """\$[a-z]+""".r     ^^ { CHAN  ( _ )  }
+  def lexInt     : Parser [ Token ] = """(0|[1-9]\d*)""".r ^^ { INT   ( _ )  }
+
+}
+
+sealed abstract class Token
+sealed abstract class InfoToken ( text: String ) extends Token
+sealed abstract class KeyWdToken extends Token { val text: String }
+
+case class  IDENT ( text: String ) extends InfoToken ( text )
+case class  CHAN  ( text: String ) extends InfoToken ( text )
+case class  INT   ( text: String ) extends InfoToken ( text )
+case class  ERROR ( text: String ) extends InfoToken ( text )
+
+case object BANG    extends KeyWdToken { val text: String = "!"       }
+case object STAR    extends KeyWdToken { val text: String = "*"       }
+case object DOT     extends KeyWdToken { val text: String = "."       }
+case object COLON   extends KeyWdToken { val text: String = ":"       }
+case object LET     extends KeyWdToken { val text: String = "let"     }
+case object NEW     extends KeyWdToken { val text: String = "new"     }
+case object IF      extends KeyWdToken { val text: String = "if"      }
+case object THEN    extends KeyWdToken { val text: String = "then"    }
+case object ELSE    extends KeyWdToken { val text: String = "else"    }
+case object ENDIF   extends KeyWdToken { val text: String = "endif"   }
+case object SEND    extends KeyWdToken { val text: String = "send"    }
+case object RECEIVE extends KeyWdToken { val text: String = "receive" }
+case object SERVER  extends KeyWdToken { val text: String = "server"  }
+case object BAR     extends KeyWdToken { val text: String = "|"       }
+case object END     extends KeyWdToken { val text: String = "end"     }
+case object LPAREN  extends KeyWdToken { val text: String = "("       }
+case object RPAREN  extends KeyWdToken { val text: String = ")"       }
+case object LCURLY  extends KeyWdToken { val text: String = "{"       }
+case object RCURLY  extends KeyWdToken { val text: String = "}"       }
+case object COMMA   extends KeyWdToken { val text: String = ","       }
+case object LARROW  extends KeyWdToken { val text: String = "<-"      }
+case object RARROW  extends KeyWdToken { val text: String = "->"      }
+case object TRUE    extends KeyWdToken { val text: String = "true"    }
+case object FALSE   extends KeyWdToken { val text: String = "false"   }
+case object PLUS    extends KeyWdToken { val text: String = "+"       }
+case object DASH    extends KeyWdToken { val text: String = "-"       }
+case object FSLASH  extends KeyWdToken { val text: String = "/"       }
+case object PERC    extends KeyWdToken { val text: String = "%"       }
+case object EQUAL   extends KeyWdToken { val text: String = "="       }
+case object EQEQ    extends KeyWdToken { val text: String = "=="      }
+case object NEQ     extends KeyWdToken { val text: String = "!="      }
+case object LESS    extends KeyWdToken { val text: String = "<"       }
+case object LESSEQ  extends KeyWdToken { val text: String = "<="      }
+case object GRTR    extends KeyWdToken { val text: String = ">"       }
+case object GRTREQ  extends KeyWdToken { val text: String = ">="      }
+case object AND     extends KeyWdToken { val text: String = "&&"      }
+case object OR      extends KeyWdToken { val text: String = "||"      }
