b8f78fb2b6dce5251f5bf0b9647a959ad6584233
Alex Jeffery <apjeffery136@googlemail.com>
2017-04-06 15:38:50 +0300

    began typeclass impl

---
+++ src/main/scala/syntax/typeclass/typeclass.scala
@@ -0,0 +1,32 @@
+package syntax.typeclass
+
+import syntax._
+
+sealed abstract class TypeClassElement extends SyntaxElement
+
+case class TypeClassDecl
+  ( val tcName : Name
+  , val bind   : Name
+  , val ty     : SChan
+  , val body   : TypeClassElement
+  ) extends TypeClassElement {
+
+  override def pstr(names: Map[Name, String]) = ???
+  override def free: Set[Name] = ???
+}
+
+case class TypeClassInst
+  ( val tcName  : Name
+  , val instTy  : SType
+  , val witness : Receive
+  , val body    : TypeClassElement
+  ) extends TypeClassElement {
+
+  override def pstr(names: Map[Name, String]) = ???
+  override def free: Set[Name] = ???
+}
+
+case class TypeClassProc ( p: Proc ) extends TypeClassElement {
+  override def pstr(names: Map[Name, String]) = p pstr names
+  override def free: Set[Name] = p.free
+}
