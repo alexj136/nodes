bd138892a9754150dc6a257af21dec4482ec4f4d
Alex Jeffery <apjeffery136@googlemail.com>
2017-04-06 14:30:55 +0300

    Continued prelude

--- src/main/scala/interpreter/prelude/prelude.scala
+++ src/main/scala/interpreter/prelude/prelude.scala
@@ -3,14 +3,24 @@ package interpreter.prelude
 import syntax._
 import interpreter._
 
-object withDefaultPrelude
-    extends Function1[(Map[Name, String], NumName, Proc), Proc] {
+object withDefaultPrelude extends Function1
+  [ (Map[Name, String], NumName, Proc)
+  , (Map[Name, String], NumName, Proc)
+  ] {
 
-  def apply(args: (Map[Name, String], NumName, Proc)) = {
-    val names : Map[Name, String] = args._1
-    val nn    : NumName           = args._2
-    val proc  : Proc              = args._3
+  def apply(args: (Map[Name, String], NumName, Proc)):
+      (Map[Name, String], NumName, Proc) = {
 
-    ???
+    val names: Map[Name, String] = args._1
+    var nn   : NumName           = args._2
+    val proc : Proc              = args._3
+
+    def n(x: Int): NumName = NumName(nn.id + x)
+    def v(x: Int): Exp     = Variable(n(x))
+
+    ( names
+    , n(2)
+    , New(n(0), SChan(List(n(1)), List(SList(SVar(n(1))))), ???)
+    )
   }
 }
